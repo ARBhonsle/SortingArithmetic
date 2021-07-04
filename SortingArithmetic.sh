d149750f0b44c6465bd1ff55ffb6af424fa6c6b9
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-05 00:10:24 +0530

    result stored in dictionary

--- SortingArithmetic.sh
+++ SortingArithmetic.sh
@@ -7,4 +7,12 @@ echo "a+b*c :$(($a+$b*$c))"
 echo "a*b+c :$(($a*$b+$c))"
 echo "c+a/b :$(($c+$a/$b))"
 echo "a%b+c :$(($a%$b+$c))"
-
+declare -A result
+result[one]=$(($a+$b*$c))
+result[two]=$(($a*$b+$c))
+result[three]=$(($c+$a/$b))
+result[four]=$(($a%$b+$c))
+for key in ${!result[@]}
+do
+	echo $key $((result[$key]))
+done
